158aadb9fedf4128069370e912326dc25cc5bf5c
Mike Goelzer <mike@goelzer.org>
2022-05-25 08:55:08 -0600

    add progress bar to wipe-server

--- cmd/extras.go
+++ cmd/extras.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/fsctl/trustlessbak/pkg/objstore"
 	"github.com/spf13/cobra"
+	"github.com/vbauerster/mpb/v7"
+	"github.com/vbauerster/mpb/v7/decor"
 )
 
 var (
@@ -86,9 +89,32 @@ func wipeServerMain() {
 	objst := objstore.NewObjStore(context.Background(), cfgEndpoint, cfgAccessKeyId, cfgSecretAccessKey)
 	ctx := context.Background()
 
+	// initialize progress bar container
+	progressBarContainer := mpb.New()
+
 	allObjects, err := objst.GetObjList(ctx, cfgBucket, "")
 	if err != nil {
 		log.Printf("error: wipeServerMain: GetObjList failed: %v", err)
+	}
+
+	// create the progress bar
+	var progressBarTotalItems int
+	var progressBar *mpb.Bar = nil
+	if !cfgVerbose {
+		progressBarTotalItems = len(allObjects)
+
+		progressBar = progressBarContainer.New(
+			int64(progressBarTotalItems),
+			mpb.BarStyle().Lbound("[").Filler("=").Tip(">").Rbound("]"),
+			mpb.PrependDecorators(
+				decor.Name("Wipe", decor.WC{W: len("Wipe") + 1, C: decor.DidentRight}),
+				// replace ETA decorator with "done" message on OnComplete event
+				decor.OnComplete(
+					decor.AverageETA(decor.ET_STYLE_GO, decor.WC{W: 4}), "done",
+				),
+			),
+			mpb.AppendDecorators(decor.Percentage()),
+		)
 	}
 
 	for objName := range allObjects {
@@ -98,8 +124,15 @@ func wipeServerMain() {
 		} else {
 			if cfgVerbose {
 				log.Printf("deleted %s\n", objName)
+			} else {
+				progressBar.Increment()
 			}
 		}
+	}
+
+	if !cfgVerbose {
+		// Give progress bar 0.1 sec to draw itself for final time
+		time.Sleep(1e8)
 	}
 }
 
