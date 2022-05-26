8f4cd56d33625547cee20d3dbe933ad80a3fa7d7
Mike Goelzer <mike@goelzer.org>
2022-05-26 07:50:56 -0600

    move env specific config to env var

--- scripts/start-minio.sh
+++ scripts/start-minio.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-if [[ $MINIO_ROOT_USER == "" || $MINIO_ROOT_PASSWORD == "" ]]; then
-    echo "Set MINIO_ROOT_USER and MINIO_ROOT_PASSWORD to your desired admin credentials"
-    echo "before running this script:"
+if [[ $MINIO_ROOT_USER == "" || $MINIO_ROOT_PASSWORD == "" || $MINIO_HOST_PORT == "" ]]; then
+    echo "Set MINIO_ROOT_USER, MINIO_ROOT_PASSWORD and MINIO_HOST_PORT to your"
+    echo "desired admin credentials before running this script:"
     echo ""
     echo "  export MINIO_ROOT_USER=minioadmin"
     echo "  export MINIO_ROOT_PASSWORD=minioadmin"
+    echo "  export MINIO_HOST_PORT=127.0.0.1:9000"
     echo "  ./scripts/start-minio.sh"
     echo ""
     exit 1
 fi
-/usr/local/opt/minio/bin/minio server --config-dir=/usr/local/etc/minio --address=127.0.0.1:9000 --console-address=127.0.0.1:9001 /usr/local/var/minio
+/usr/local/opt/minio/bin/minio server --config-dir=/usr/local/etc/minio --address=$MINIO_HOST_PORT --console-address=127.0.0.1:9001 /usr/local/var/minio
 
