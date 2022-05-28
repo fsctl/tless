b93b632543f10ee41a445a7644643b1d49c6ee0c
Mike Goelzer <mike@goelzer.org>
2022-05-28 13:57:54 -0600

    make daemon mode independent of viper/cobra/config.toml

--- daemon/main.go
+++ daemon/main.go
@@ -1,4 +1,4 @@
-package cmd
+package daemon
 
 import (
 	"context"
@@ -10,32 +10,11 @@ import (
 	"syscall"
 
 	pb "github.com/fsctl/tless/rpc"
-	"github.com/spf13/cobra"
 	"google.golang.org/grpc"
 )
 
 const (
 	unixSocketPath = "/tmp/tless.sock"
-)
-
-var (
-	daemonCmd = &cobra.Command{
-		Use:   "daemon",
-		Short: "Runs as a background daemon",
-		Long: `Starts the program in a mode where it just sits and waits for SIGINT or SIGTERM. 
-While waiting, the daemon will listen on the socket /tmp/tless.sock for commands and 
-return the results of running those commands through the socket. Logging output will still 
-be written to STDERR.
-
-Example:
-
-	tless daemon
-`,
-		Args: cobra.NoArgs,
-		Run: func(cmd *cobra.Command, args []string) {
-			daemonMain()
-		},
-	}
 )
 
 // server is used to implement helloworld.GreeterServer.
@@ -49,18 +28,13 @@ func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloRe
 	return &pb.HelloReply{Message: "Hello from " + in.GetName()}, nil
 }
 
-func init() {
-	rootCmd.AddCommand(daemonCmd)
-}
-
-func daemonMain() {
+func DaemonMain() {
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
 	done := make(chan bool, 1)
 
 	go func() {
 		// setup listener and grpc server instance
-		//lis, err := net.Listen("tcp4", fmt.Sprintf("127.0.0.1:%d", cfgPort))
 		lis, err := net.Listen("unix", unixSocketPath)
 		if err != nil {
 			log.Fatalf("failed to listen: %v", err)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 
 	"github.com/fsctl/tless/cmd"
+	"github.com/fsctl/tless/daemon"
 )
 
 var (
@@ -19,6 +20,10 @@ func main() {
 	// during `go build`
 	if len(os.Args) == 2 && (os.Args[1] == "--version" || os.Args[1] == "version") {
 		fmt.Printf("tless v%s-%s (built at %s)\n", Version, CommitHash, BuildTimestamp)
+	} else if len(os.Args) == 2 && (os.Args[1] == "daemon" || os.Args[1] == "-d") {
+		// We also trap 'daemon'/'-d' out here because daemon mode bypasses all the viper/cobra
+		// and toml config stuff because in this mode we receive all our input on a grpc unix socket.
+		daemon.DaemonMain()
 	} else {
 		cmd.Execute()
 	}
